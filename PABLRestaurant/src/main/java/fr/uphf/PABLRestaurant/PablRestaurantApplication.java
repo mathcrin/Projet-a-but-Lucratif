ac44af5c219b86ddfdfa8c8f638458380ac9129d
mathcrin <mathcrin@gmail.com>
2024-04-06 23:12:01 +0200

    Corection du bug qui empéchait d'utilisé les controller de Client et Restaurant

---
+++ PABLRestaurant/src/main/java/fr/uphf/PABLRestaurant/PablRestaurantApplication.java
@@ -0,0 +1,13 @@
+package fr.uphf.PABLRestaurant;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PablRestaurantApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PablRestaurantApplication.class, args);
+	}
+
+}
---
+++ PABLRestaurant/src/test/java/fr/uphf/PABLRestaurant/PablRestaurantApplicationTests.java
@@ -0,0 +1,13 @@
+package fr.uphf.PABLRestaurant;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PablRestaurantApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
