8eca7907f5d3e7abecd68104d8cd17c8044c3dff
Nath <nathaan59@gmail.com>
2024-04-07 23:08:04 +0200

    Utilisation des collection avec les token auth2.0

--- Gateway/src/main/java/fr/uphf/gateway/SecurityFilter.java
+++ Gateway/src/main/java/fr/uphf/gateway/SecurityFilter.java
@@ -1,4 +1,27 @@
 package fr.uphf.gateway;
 
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.method.configuration.EnableReactiveMethodSecurity;
+import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
+import org.springframework.security.config.web.server.ServerHttpSecurity;
+import org.springframework.security.web.server.SecurityWebFilterChain;
+
+@Configuration
+@EnableReactiveMethodSecurity
+@EnableWebFluxSecurity
 public class SecurityFilter {
+
+    @Bean
+    public SecurityWebFilterChain springSecurityFilterChain(ServerHttpSecurity http) {
+        http.authorizeExchange(authorize -> authorize
+                        .pathMatchers(HttpMethod.GET, "/commandes/**", "/clients/**", "/restaurants/**")
+                        .hasAuthority("SCOPE_all")
+                        .pathMatchers(HttpMethod.POST, "/commandes/**", "/clients/**", "/restaurants/**")
+                        .hasAuthority("SCOPE_all")
+                        .anyExchange().authenticated())
+                .oauth2ResourceServer(ServerHttpSecurity.OAuth2ResourceServerSpec::jwt);
+        return http.build();
+    }
 }
