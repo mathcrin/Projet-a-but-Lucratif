9a8c97c918ae8a14a09fd150c14e90524bd08b2d
Nath <nathaan59@gmail.com>
2024-04-28 16:45:44 +0200

    Utilisation des collection avec les token auth2.0

---
+++ PABLRestaurant/src/main/java/fr/uphf/PABLRestaurant/entities/RestaurantEntity.java
@@ -0,0 +1,33 @@
+package fr.uphf.PABLRestaurant.entities;
+
+
+import jakarta.persistence.*;
+        import lombok.*;
+
+@Entity
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+@Table(name = "restaurant")
+public class RestaurantEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE)
+    @Column(name = "id", nullable = false)
+    private Integer id;
+
+    @Column(nullable = false)
+    private String description;
+
+    @Column(nullable = false)
+    private String adresse;
+
+    @Column(nullable = false)
+    private String mail;
+
+    @Column(nullable = false)
+    private String telephone;
+
+
+}
