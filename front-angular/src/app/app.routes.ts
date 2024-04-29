9c28f5d0846c9b1daaebb76b899d42d19a532ac1
Nath <nathaan59@gmail.com>
2024-04-29 16:37:58 +0200

    BUG du CORS Policy qui nous bloque

--- front-angular/src/app/app.routes.ts
+++ front-angular/src/app/app.routes.ts
@@ -1,8 +1,10 @@
 import { Routes } from '@angular/router';
 import { CommandePageComponent } from './commande-page/commande-page.component';
 import {AcceuilComponent} from "./acceuil/acceuil.component";
+import {FormComponent} from "./formulaire/formulaire.component";
 
 export const routes: Routes = [
   { path: '', component: AcceuilComponent },
   { path: 'commander', component: CommandePageComponent },
+  { path: 'form', component: FormComponent },
 ];
